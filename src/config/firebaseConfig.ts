716e666f07d2afc605f24aba9de8d6c98d795eab
Jorge Daniel Sosa <jdsosa@gmail.com>
2024-10-30 10:28:25 +0000

    Feat: Initial db query

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAlPmQOk3XpwEJ7L3mtRtmd2doJP_fb5k8',
+  authDomain: 'jdsosa-testing-nest.firebaseapp.com',
+  projectId: 'jdsosa-testing-nest',
+  storageBucket: 'jdsosa-testing-nest.appspot.com',
+  messagingSenderId: '95764591168',
+  appId: '1:95764591168:web:5a16c6f7f789cc5d4ee5a5',
+}
--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,16 +1,33 @@
+import { firebaseConfig } from '@/config/firebaseConfig'
 import type { Product } from '@/products/domain/Product'
-import { productsFixture } from '@/products/infrastructure/fixtures/productsFixture'
+import { initializeApp } from 'firebase/app'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { collection, getDocs, getFirestore, query } from 'firebase/firestore'
 
 type Data = {
   products: Product[]
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+enum FirebaseCollections {
+  PRODUCTS = 'products',
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.setHeader('Content-Type', 'application/json')
   res.setHeader('Access-Control-Allow-Methods', 'POST')
 
-  res.status(200).json({ products: productsFixture })
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app)
+
+  const collectionRef = collection(db, FirebaseCollections.PRODUCTS)
+
+  const queryRef = query(collectionRef)
+
+  const products = (await getDocs(queryRef).then((data) =>
+    data.docs.map((doc) => doc.data()),
+  )) as Product[]
+
+  res.status(200).json({ products })
 
   return
 }
